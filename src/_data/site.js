66a9af3f3a17d698b571fcf9b29fe564670c78c2
Simon Whatley <simonwhatley@gmail.com>
2024-11-06 17:03:36 +0000

    Update site data

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
-  name: 'GOV.UK Service Assessments',
-  title: 'GOV.UK Service Assessments',
+  name: 'Service Assessments',
+  title: 'Service Assessments',
   description: '',
   author: 'Simon Whatley',
   email: 'hello@simonwhatley.co.uk'
